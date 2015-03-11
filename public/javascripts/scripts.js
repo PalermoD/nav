7028d4bb8503108a6696ffdafbc30c56af75bee8
PalermoD <palermo.deschamps@gmail.com>
2015-03-11 19:19:22 -0400

    added more behaviors to nav bar

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,7 +1,46 @@
-$(function() {
-    $('#nav-wrapper').height($("#nav").height());
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
     
-    $('#nav').affix({
-        offset: { top: $('#nav').offset().top }
-    });
+    
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        
+        $('div').removeClass('nav-down').addClass('nav-up');
+    } else {
+        
+        if(st + $(window).height() < $(document).height()) {
+            $('div').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+$(function() {
+$('#button').on('click', function() {
+    
+    console.log('i clicked');
+    $('div').removeClass('nav-down').addClass('nav-up');
+});
+
 });
